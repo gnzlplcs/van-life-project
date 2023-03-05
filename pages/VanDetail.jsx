5d3466fb364bd9956e306d3f793aab615ca48b8e
gnzlplcs <palaciosgm@outlook.com>
2023-03-04 23:41:59 -0500

    :construction: route params 3.1 useParams()

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const VanDetail = () => {
+  const params = useParams();
+
+  console.log(params);
+
   return <h1>Van Detail goes here</h1>;
 };
 
