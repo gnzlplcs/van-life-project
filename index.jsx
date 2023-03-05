b83f3534892f5384a7a7e4ba982fb38c65e55e9b
gnzlplcs <palaciosgm@outlook.com>
2023-03-04 22:58:03 -0500

    :construction: route params part 1

--- index.jsx
+++ index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetail";
 
 import "./server";
 
@@ -23,6 +24,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
       <footer className="footer">
         <p>Ⓒ 2022 #VANLIFE</p>
---
+++ pages/VanDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VanDetail = () => {
+  return <h1>Van Detail goes here</h1>;
+};
+
+export default VanDetail;
