6692d6c1c32c9c9be70694bcbf3fded2d6ef0991
gnzlplcs <palaciosgm@outlook.com>
2023-03-06 00:18:23 -0500

    :construction: params 3.2 copleted

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,12 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "../server";
 
 const VanDetail = () => {
-  useEffect(() => {}, []);
-    fetch(`/api/vans/${params.id}`).then(res => res.json()).then(data=> console.log(data))
+  const params = useParams();
+  const [van, setVan] = useState(null)
 
-  return <h1>Van Detail goes here</h1>;
+  useEffect(() => {
+    fetch(`/api/vans/${params.id}`).then(res => res.json()).then(data=> setVan(data.vans))
+
+  }, [params.id]);
+
+  return (
+    <div className="van-detail-container">
+    {van ? (
+      <div className="van-detail">
+        <img src={van.imageUrl}/>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        <h2>{van.name}</h2>
+        <p className="van-price"><span>${van.price}</span>/day</p>
+        <p>{van.description}</p>
+        <button className="link-button">Rent this van</button>
+      </div>
+    ) : <h2>Loading...</h2>}
+
+    </div>
+  );
 };
 
 export default VanDetail;
