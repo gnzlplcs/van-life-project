fe6b7993974fbc276a5c85ceabb212b73f68ed23
gnzlplcs <palaciosgm@outlook.com>
2023-03-06 00:05:28 -0500

    :construction: react router 3.2 challenge

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "../server";
 
 const VanDetail = () => {
-  const params = useParams();
-
-  console.log(params);
+  useEffect(() => {}, []);
+    fetch(`/api/vans/${params.id}`).then(res => res.json()).then(data=> console.log(data))
 
   return <h1>Van Detail goes here</h1>;
 };
