d4c3d799f7631855ba9b9cf6d0b4865b4a84d1fb
gnzlplcs <palaciosgm@outlook.com>
2023-03-07 07:58:47 -0500

    :construction: index routes

--- index.jsx
+++ index.jsx
@@ -16,17 +16,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
 
           <Route path="host" element={<HostLayout />}>
-            <Route path="host" element={<Dashboard />} />
+            <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
+          
         </Route>
       </Routes>
     </BrowserRouter>
